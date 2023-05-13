5572374907360d82019fb234f9b35f033ad9aee0
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-05-13 23:56:35 +0800

    added directions

---
+++ tv-oop.py
@@ -0,0 +1,4 @@
+# Given: A UML Class Diagram: 
+# Required: Create a Python Code for creating the Class named TV and a Test Driver program named TestTV that 
+# will create two objects from Class TV and will #produce the following output:
+
