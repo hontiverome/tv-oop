da7f61a9ba7e584d7433132c57a704dac7638942
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-05-16 00:09:46 +0800

    boolean tests

--- television.py
+++ television.py
@@ -17,12 +17,11 @@ class TV:
     # turn_on
     def turn_on(self):
         self.on=True
-        return True
         
     # turn_off
     def turn_off(self):
         self.on=False
-        return False
+
         
     # get_channel
     def get_channel(self):
