a59af825163b2fcc965bcf85550613b9dd729408
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-05-16 00:01:07 +0800

    compiling television 1

--- test-tv.py
+++ test-tv.py
@@ -2,3 +2,6 @@ from television import TV
 
 # print the outputs: tv1's channel is 30 and volume level is 3, tv2's channel is 3 and volume level is 2
 television1=TV(1,1)
+television1.turn_on()
+television1.set_channel(30)
+television1.set_volume(3)
