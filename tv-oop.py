7d78342a39c9968e910f89e8387c0842046ada7d
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-05-15 19:40:41 +0800

    added more directions

--- tv-oop.py
+++ tv-oop.py
@@ -1,4 +1,5 @@
 # Given: A UML Class Diagram: 
 # Required: Create a Python Code for creating the Class named TV and a Test Driver program named TestTV that 
 # will create two objects from Class TV and will #produce the following output:
-
+# tv1's channel is 30 and volume level is 3
+# tv2's channel is 3 and volume level is 2
